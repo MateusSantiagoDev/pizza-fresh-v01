72fe68f1ff78c92b0551c9fa5e49de7b77672921
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-11-15 14:00:22 -0300

    feat(product): implement Dto validation

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,6 +1,34 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNumber, IsString, IsUrl } from "class-validator";
+
 export class CreateProductDto {
+  @IsString()
+  @ApiProperty({
+    description: "Nome do produto",
+    example: "Pizza de mussarela",
+  })
   name: string;
+
+  @IsString()
+  @ApiProperty({
+    description: "Descrição do produto",
+    example: "Queijo mussarela e borda recheda de catupiry",
+  })
   description: string;
+
+  @IsNumber({
+    maxDecimalPlaces: 2,
+  })
+  @ApiProperty({
+    description: "Preço do produto",
+    example: 22.35,
+  })
   price: number;
+
+  @IsUrl()
+  @ApiProperty({
+    description: "Imagem do produto",
+    example: "https://fotodepizza.com",
+  })
   image: string;
 }
