606840a8c49fd90374d457714bc4af83ef468c48
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-11-15 11:29:17 -0300

    feat(db): add model for "product" & "user"

--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { Injectable, NotFoundException, UnprocessableEntityException } from "@nestjs/common";
 import { PrismaService } from "src/prisma/prisma.service";
 import { CreateTableDto } from "./dto/create-table.dto";
 import { UpdateTableDto } from "./dto/update-table.dto";
@@ -28,18 +28,24 @@ export class TableService {
 
     create(dto: CreateTableDto): Promise<Table> {
        const data: Table = { ...dto };
-       return this.prisma.table.create({ data })
+       return this.prisma.table.create({ data }).catch(this.handleError);
     }
 
     async update(id: string, dto: UpdateTableDto): Promise<Table> {
       await this.findById(id)
       const data: Partial<Table> = { ...dto }; // partial faz a mesma coisa q o partialTypes
-      return this.prisma.table.update({ where: { id }, data });
+      return this.prisma.table.update({ where: { id }, data }).catch(this.handleError);
     }
 
     async delete(id: string) {
       await this.findById(id)
       await this.prisma.table.delete({ where: { id } })
+    }
+
+    handleError(err: Error): undefined {
+      const errorLines = err.message?.split("\n"); // combinação para melhorar a vizualização do erro
+      const lastErrorLine = errorLines[errorLines.length -1]?.trim(); // complemento com a linha de cima
+      throw new UnprocessableEntityException(lastErrorLine || "Algum erro ocorreu ao executar a operação") // vai exibir o erro no swagger
     }
 
 }
