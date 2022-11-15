53817e9fd8946c6a8caf6ee7bb1e3828361235dc
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-11-15 13:49:47 -0300

    feat(product): implement dto & service

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,26 +1,51 @@
-import { Injectable } from '@nestjs/common';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
+import { Injectable, NotFoundException, UnprocessableEntityException } from "@nestjs/common";
+import { PrismaService } from "src/prisma/prisma.service";
+import { CreateProductDto } from "./dto/create-product.dto";
+import { UpdateProductDto } from "./dto/update-product.dto";
+import { Product } from "./entities/product.entity";
 
 @Injectable()
 export class ProductService {
-  create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
-  }
 
-  findAll() {
-    return `This action returns all product`;
-  }
+    constructor(private readonly prisma: PrismaService) {}
 
-  findOne(id: string) {
-    return `This action returns a #${id} product`;
-  }
+    findAll() {
+        return this.prisma.product.findMany()
+    }
 
-  update(id: string, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
-  }
+    async findById(id: string): Promise<Product> {
+      const data = await this.prisma.product.findUnique({ where: { id } });
+      if(!data) {
+        throw new NotFoundException(`não foi encontrado nenhum registro com o ID: ${id}`)
+      }
+      return data;
 
-  delete(id: string) {
-    return `This action removes a #${id} product`;
-  }
+    }
+
+    async findOne(id: string) {
+      return await this.findById(id)
+    }
+
+    create(dto: CreateProductDto): Promise<Product> {
+       const data: Product = { ...dto };
+       return this.prisma.product.create({ data }).catch(this.handleError);
+    }
+
+    async update(id: string, dto: UpdateProductDto): Promise<Product> {
+      await this.findById(id)
+      const data: Partial<Product> = { ...dto }; // pnumberartial faz a mesma coisa q o partialTypes
+      return this.prisma.product.update({ where: { id }, data }).catch(this.handleError);
+    }
+
+    async delete(id: string) {
+      await this.findById(id)
+      await this.prisma.product.delete({ where: { id } })
+    }
+
+    handleError(err: Error): undefined {
+      const errorLines = err.message?.split("\n"); // combinação para melhorar a vizualização do erro
+      const lastErrorLine = errorLines[errorLines.length -1]?.trim(); // complemento com a linha de cima
+      throw new UnprocessableEntityException(lastErrorLine || "Algum erro ocorreu ao executar a operação") // vai exibir o erro no swagger
+    }
+
 }
