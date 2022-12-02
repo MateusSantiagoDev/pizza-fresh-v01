e392c0da284bde945a488c1c049a87c8c1f59ba4
Mateus Santiago <mateus_santiago2.3@outlook.com>
2022-12-02 13:49:02 -0300

    feat(user): implement dto & entity

---
+++ src/user/dto/create-user.dto.ts
@@ -0,0 +1,42 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsString, IsUrl, Matches, MinLength } from "class-validator";
+
+export class CreateUserDto {
+  @IsString()
+  @ApiProperty({
+    description: "Nome do usuário. UTILIZADO no login. Deve ser único",
+    example: "mateussantiago"
+  })
+  nickname: string;
+
+  @IsString()
+  @ApiProperty({
+    description: "Nome do usuário. Apenas para exibição",
+    example: "Mateus Santiago"
+  })
+  name: string;
+
+  @IsString()
+  @MinLength(6)
+  @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message: 'Senha muito fraca',
+  })
+  @ApiProperty({
+    description: "Senha do usuario para login",
+    example: "abcd1234"
+  })
+  password: string;
+
+  @ApiProperty({
+    description: "A confirmação da senha deve ser igual a senha",
+    example: "abcd1234"
+  })
+  confirmPassword: string;
+
+  @IsUrl()
+  @ApiProperty({
+    description: "Imagem de perfil do usuário",
+    example: "https://avatars.githubusercontent.com/u/106131007?v=4"
+  })
+  image: string;
+}
